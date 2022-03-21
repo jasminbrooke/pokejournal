795e990747af7128cabdc889fb5e69439156f893
+ <+>
2022-03-21 00:51:57 -0400

    add date, field notes, styling

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+    const dateDisplay = document.getElementById("date")
     const form = document.getElementById("form")
     const userInput = document.getElementById("search")
 
+    const displayDate = () => {
+        const today = new Date
+        const month = monthNames[today.getMonth()]
+        const day = today.getDate()
+        dateDisplay.innerText = `${month} ${day}`
+    }
+
     const getPokemon = (pokemon) => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}/`)
         .then(res => res.json())
         .then(data => renderPokemon(data))
 
@@ -16,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const name = document.createElement("h3")
         const types = document.createElement("p")
         const sprite = document.createElement("img")
-        name.innerText = data.name
+        name.innerText = data.name.toUpperCase()
 
         sprite.setAttribute("src", data.sprites.front_default)
         sprite.addEventListener("click", () => {
@@ -31,13 +42,41 @@ document.addEventListener("DOMContentLoaded", () => {
         card.appendChild(name)
         card.appendChild(types)
         card.appendChild(sprite)
+
+        const notes = document.createElement("form")
+        card.appendChild(notes)
+        notes.setAttribute("action", "#")
+        notes.setAttribute("id", "notes")
+        const textField = document.createElement("input")
+        textField.setAttribute("type", "text")
+        textField.setAttribute("placeholder", "Add field notes...")
+        const button = document.createElement("input")
+        button.setAttribute("type", "submit")
+        button.setAttribute("value", "Add")
+
+        
+        notes.appendChild(textField)
+        notes.appendChild(button)
+        const ul = document.createElement("ul")
+        card.appendChild(ul)
+        
+        notes.addEventListener("submit", (e) => {
+            e.preventDefault()
+            const li = document.createElement("li")
+            li.innerText = textField.value
+            ul.appendChild(li)
+            notes.reset()
+
+        })
     }
 
     form.addEventListener("submit", (e) => {
         e.preventDefault()
         getPokemon(userInput.value)
+        form.reset()
+        
     })
 
-
     getPokemon("pikachu")
+    displayDate()
 })
